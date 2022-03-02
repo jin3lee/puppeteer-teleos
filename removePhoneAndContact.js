10e82c7d45f612f602eefaa08ad1819c0e073b0b
Joseph Lee <joseph@teleosmarketing.com>
2022-03-02 16:21:50 -0600

    takes a list of websites and automates

--- removePhoneAndContact.js
+++ removePhoneAndContact.js
@@ -1,79 +1,83 @@
 const puppeteer = require('puppeteer');
+const websiteList = [ 
+];
+
+// const cars = ["Saab", "Volvo", "BMW"];
 
 (async () => {
 
-
-    const args = process.argv.slice(2);
-    var websiteUrl = args[0]; // website
-    var adminPortal = "https://" + websiteUrl.replaceAll("https://", "").replaceAll("/","") + "/wp-admin";
-
-    // login credentials
-    const email = "joseph@teleosmarketing.com";
-    const password = "DMHceRzn7mEx";
-
-    // open site
     const browser = await puppeteer.launch({ headless: false });
-    const pages = await browser.pages();
-    var page = pages[0];
-    await page.goto(adminPortal);
 
-    // email
-    await page.waitForSelector("#user_login");
-    await page.type("#user_login", email);
+    for( var website in websiteList ) {
+      var websiteUrl = websiteList[website]; // website
+      var adminPortal = "https://" + websiteUrl.replaceAll("https://", "").replaceAll("/","") + "/wp-admin";
 
-    // password
-    await page.type("#user_pass", password);
+      // login credentials
+      const email = "joseph@teleosmarketing.com";
+      const password = "DMHceRzn7mEx";
 
-    // click submit
-    await page.click("#wp-submit");
-    
-    // go to 
-    await page.waitForSelector(".ab-icon");
-    await page.goto(adminPortal + "/nav-menus.php"); 
+      // open site
+      var page = await browser.newPage();
+      await page.goto(adminPortal);
 
-    // click delete -109 - CONTACT
-    await page.waitForSelector("#select-menu-to-edit");
-    await page.click("#edit-109");
-    await page.waitForTimeout(1500);  // wait to click delete contact
-    await page.waitForSelector("#delete-109");
-    await page.click("#delete-109");
-    console.log(".........deleted contact");
-    
-    // * 438 - SNF
-    // * 2559 - MEM - Granbury
-    // Delete - 438 - PHONENUMBER
-    await page.waitForSelector("#edit-438");
-    await page.click("#edit-438");
-    await page.waitForTimeout(1500);  // wait to click delete phone
-    await page.waitForSelector("#delete-438");
-    await page.click("#delete-438");
-    console.log(".........deleted phonenumber");
+      // email
+      await page.waitForSelector("#user_login");
+      await page.type("#user_login", email);
 
-    // remove connect page
-    await page.waitForSelector("#edit-61");
-    await page.click("#edit-61");
-    await page.waitForTimeout(1500);  // wait to click delete connect
-    await page.waitForSelector("#delete-61");
-    await page.click("#delete-61");
-    console.log(".........deleted connect page");
+      // password
+      await page.type("#user_pass", password);
 
-    // click left sidebar page accordian li
-    await page.click("#add-post-type-page");
-    await page.waitForTimeout(1500);  // wait for accordian to open
-    await page.evaluate(() => {
-      let checkbox = document.querySelector(".menu-item-checkbox");
-      checkbox.checked = true;
-      console.log("checked the box", checkbox);
-    });
+      // click submit
+      await page.click("#wp-submit");
+      
+      // go to 
+      await page.waitForSelector(".ab-icon");
+      await page.goto(adminPortal + "/nav-menus.php"); 
 
-    // add to menu button
-    await page.click("#submit-posttype-page");
-    console.log("add connect to menu");
+      // click delete -109 - CONTACT
+      await page.waitForSelector("#select-menu-to-edit");
+      await page.click("#edit-109");
+      await page.waitForTimeout(1500);  // wait to click delete contact
+      await page.waitForSelector("#delete-109");
+      await page.click("#delete-109");
+      console.log(".........deleted contact");
+      
+      // * 438 - SNF
+      // * 2559 - MEM - Granbury
+      // Delete - 438 - PHONENUMBER
+      await page.waitForSelector("#edit-438");
+      await page.click("#edit-438");
+      await page.waitForTimeout(1500);  // wait to click delete phone
+      await page.waitForSelector("#delete-438");
+      await page.click("#delete-438");
+      console.log(".........deleted phonenumber");
 
-    // save menu
-    await page.waitForTimeout(3000);  
-    await page.click("#save_menu_footer");
-    console.log("SAVED MENU WOHOO!!!!!!!!!!!!!!!");
+      // remove connect page
+      await page.waitForSelector("#edit-61");
+      await page.click("#edit-61");
+      await page.waitForTimeout(1500);  // wait to click delete connect
+      await page.waitForSelector("#delete-61");
+      await page.click("#delete-61");
+      console.log(".........deleted connect page");
 
+      // click left sidebar page accordian li
+      await page.click("#add-post-type-page");
+      await page.waitForTimeout(1500);  // wait for accordian to open
+      await page.evaluate(() => {
+        let checkbox = document.querySelector(".menu-item-checkbox");
+        checkbox.checked = true;
+        console.log("checked the box", checkbox);
+      });
+
+      // add to menu button
+      await page.click("#submit-posttype-page");
+      console.log("add connect to menu");
+
+      // save menu
+      await page.waitForTimeout(3000);  
+      await page.click("#save_menu_footer");
+      await page.waitForTimeout(10000);  
+      console.log("COMPLETED ", websiteUrl, "!!!!!!!!!!!!!!!!!!!");
+    }
   //await browser.close();
 })();
